c6b10419341003ea01f7f7a4275f86ed9d248cf8
Jon Myrick <jonmyrick7401@gmail.com>
2018-11-10 12:35:42 -0700

    updated mock data

--- dbMockData.js
+++ dbMockData.js
@@ -4,11 +4,14 @@ const _ = require('lodash');
 
 module.exports = (async () => {
   try {
-    const users = [];
+    const users = [
+      { firstName: 'Jon', lastName: 'Myrick' },
+      { firstName: 'James', lastName: 'Carlson' },
+      { firstName: 'Keith', lastName: 'Halterman' },
+    ];
     const items = [];
 
-    _.times(10, () => {
-      users.push({ firstName: faker.name.firstName(), lastName: faker.name.lastName() })
+    _.times(20, () => {
       items.push({ name: faker.commerce.productName(), price: faker.commerce.price() })
     })
 
@@ -26,61 +29,7 @@ module.exports = (async () => {
     await order1.addItems(2);
     await order1.addItems(3);
 
-    // const user1 = await  db.models.user.findOne({
-    //   where: { id: 1 },
-    //   attributes: ['firstName', 'lastName'],
-    //   include: {
-    //     model: db.models.order,
-    //     attributes: ['id', 'complete'],
-    //     include: {
-    //       model: db.models.items,
-    //       attributes: ['id', 'name', 'price'],
-    //     }
-    //   }
-    // });
-
-    console.log('auto filled mock data')
-
-    // const orders = await user1.getOrders();
-    // console.log(orders)
-
-
-
-
-    // const orders = await Order.findOne({
-    //   where: { id: 1 },
-    //   attributes: ['id', 'complete'],
-    //   include: [{
-    //     model: Items,
-    //     attributes: ['id', 'name'],
-    //   }],
-    // });
-
-    //const items = await order1.getItems();
-    // const stuff = items.map(e => e.toJSON());
-    // console.log('items', stuff);
-
-    // const items = await Item.findAll({
-    //   attributes: ['id', 'name'],
-    //   include: [{all: true}]
-    // });
-
-    // const data = orders.map((orderDoc) => {
-    //   const order = orderDoc.toJSON();
-    //   order.items = order.items.map(item => item.id);
-    //   return order;
-    // })
-
-    // console.log('orders', JSON.stringify(orders, null, 2));
-
-
-
-    //  const orders = await Order.find({
-    //   where: { userId: 1 },
-    //   attributes: ['id', 'userId', 'complete'],
-    //   include: [{all: true}],
-    //   raw: true,
-    // });
+    console.log('auto-filled mock data')
   } catch (error) {
     console.log(error);
   }
